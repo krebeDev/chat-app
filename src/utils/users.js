b36e621f1be8e820af2adb4146b161b5b76ed77c
Solomon Ekrebe <krebedev@gmail.com>
2020-08-06 17:24:52 +0100

    initial commit

---
+++ src/utils/users.js
@@ -0,0 +1,52 @@
+const users = []
+
+// addUser, removeUser, getUser, getUSersInRoom
+
+const addUser = ({ id, username, room }) => {
+	// Clean the data
+	username = username.trim().toLowerCase()
+	room = room.trim().toLowerCase()
+
+	// Validatet the data
+	if (!username || !room) {
+		return {
+			error: 'Username and room are required!',
+		}
+	}
+
+	// Check existing user
+	const existingUser = users.find((user) => {
+		return user.room === room && user.username === username
+	})
+
+	// Validdate username
+	if (existingUser) {
+		return {
+			error: 'Username is in use',
+		}
+	}
+
+	// Store user
+	const user = { id, username, room }
+	users.push(user)
+	return { user }
+}
+
+const removeUser = (id) => {
+	const index = users.findIndex((user) => user.id === id)
+
+	if (index !== -1) {
+		return users.splice(index, 1)[0]
+	}
+}
+
+const getUser = (id) => users.find((user) => user.id === id)
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room)
+
+module.exports = {
+	addUser,
+	removeUser,
+	getUser,
+	getUsersInRoom,
+}
